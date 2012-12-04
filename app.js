1c5a20c6529455e708dfe45607708a488df839ab
Dmitriev Alexey <dmitriew.alexej@gmail.com>
2012-12-04 13:48:30 +0300

    new blog

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-var express = require('express');
-var app = express();
+var port = process.env.PORT || 5000;
 
-app.configure(function() {
-    app.engine('html', require('uinexpress').__express) // Используем функцию "template" библиотеки underscore для рендеринга
-    app.set('view engine', 'html')                      
-    app.set('views', __dirname + "/tpl");
-    app.set("view options", {layout: 'layout.html'});   // Файл layout.html по умолчанию будет оборачивать все шаблоны
-    app.use(express.static(__dirname + "/public"));     // Делаем файлы из папки public доступными на сайте
-});
+var docpadInstanceConfiguration = {
+    templateData: {
+        site: {
+            title: "Сайт и блог программиста",
+            url: "http://blog.programmer-site.tk"
+        }
+      },
+      getTitle: function() {
+          if (this.document.title) {
+              return "" + this.document.title + " [" + this.site.title + "]";
+          } else {
+              return this.site.title;
+          }
+      },
+      getDescription: function() {
+          if (this.document.description) {
+            return this.document.description;
+          }
+          return "Пример сайта и блога программиста";
+      },
+      collections: {
+          posts: function(database) {
+              return database.findAll({
+                relativeOutDirPath: 'posts'
+              }, [{
+                date: -1
+              }]);
+          }
+      },
+      port: port
+};
 
-app.get('/', function(req, res){          // Обрабатываем запрос корневой страницы "/"
-    res.render('index.html');
+var docpadInstance = require('docpad').createInstance(docpadInstanceConfiguration, function(err) {
+    if (err) {
+        return console.log(err, err.stack);
+    }
+    return docpadInstance.action('generate server', function(err) {
+        if (err) {
+          return console.log(err, err.stack);
+        }
+        return console.log("Running on @:" + port);
+    });
 });
-app.get('/portfolio', function(req, res){ // Обрабатываем запрос страницы "/portfolio"
-    res.render('portfolio.html');
-});
-
-var port = process.env.PORT || 5000;       
-app.listen(port)                           // Запускаем сервер на 5000 порту, если не указана переменная окружения "port" 
-console.log("Listening at " + port)        // Пишем в консоль, что запустились
